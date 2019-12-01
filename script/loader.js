29ec62fe697f039b2abd07ff4c2d78521c802441
Horyong Jung <jhoryong@gmail.com>
2019-12-02 04:19:06 +0900

    update

--- script/loader.js
+++ script/loader.js
@@ -1,15 +1,18 @@
 // fade-in to loader and fadeout
-
+window.onload = function() {
 var loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart; 
     //alert('Page load time is '+ loadTime);
     if (loadTime < 5000) {
         setTimeout(function() {$('.preloader').hide();}, 5000)
         setTimeout(function() {$('.fade-box').fadeIn(1000).fadeOut(2000);}, 4000);
-        setTimeout(function() {$('.dialogue-0, .dialogue-0 .line, .dialogue-box').show();}, 7000);
+        start();
+        //setTimeout(function() {$('.dialogue-0, .dialogue-0 .line, .dialogue-box').show();}, 7000);
     } else {
         window.addEventListener("load", function() {
             setTimeout(function() {$('.preloader').hide();}, 1000);
             $('.fade-box').fadeIn(1000).fadeOut(2000);
-            setTimeout(function() {$('.dialogue-0-1, .dialogue-0-1 .line, .dialogue-box').show();}, 3000);
+            start();
+            //setTimeout(function() {$('.dialogue-0-1, .dialogue-0-1 .line, .dialogue-box').show();}, 3000);
         })
     }
+}
